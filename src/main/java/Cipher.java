cb485e7fa87b67c69fd065aa2f5fc559130fc27f
WairimuThuku <lucythuku33@gmail.com>
2022-02-13 17:16:21 +0300

    first declaration

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,2 +1,8 @@
 public class Cipher {
+    private String plainWord;
+    private int shift;
+    public Cipher(String plainWord, int shift){
+        this.plainWord = plainWord;
+        this.shift = shift;
+    }
 }
