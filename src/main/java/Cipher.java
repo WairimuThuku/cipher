62ae8978c05841d2d376f004da1dffc5ea8355ff
WairimuThuku <lucythuku33@gmail.com>
2022-02-13 17:18:11 +0300

    update declaration

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -12,4 +12,10 @@ public class Cipher {
     public int getShift(){
         return shift;
     }
+    public static String encrypt(String plainWord, int shift){
+        if(shift > 26){
+            shift = shift % 26;
+        } else if (shift < 0){
+            shift = (shift % 26) + 26;
+        }
 }
