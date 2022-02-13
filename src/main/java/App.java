aa820fc6cd376bbbda91078335c4a159da77d810
WairimuThuku <lucythuku33@gmail.com>
2022-02-13 17:12:59 +0300

    first declaration

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,8 @@
 public class App {
-}
+    public  static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean runningProgram = true;
+        while (runningProgram) {
+            System.out.println("Hey there, this is The Ceasar Cipher, you can choose to encrypt a word, decrypt a word or exit the program. Enter Encrypt, Decrypt or Exit");
+            String directionChoice = input.next();
+
