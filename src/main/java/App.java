d579818a39f2d9215d17a73207b209d483c58613
WairimuThuku <lucythuku33@gmail.com>
2022-02-13 17:14:23 +0300

    encryption declaration

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,3 +6,18 @@ public class App {
             System.out.println("Hey there, this is The Ceasar Cipher, you can choose to encrypt a word, decrypt a word or exit the program. Enter Encrypt, Decrypt or Exit");
             String directionChoice = input.next();
 
+
+            if (directionChoice.equals("Encrypt")) {
+
+                System.out.println("Enter a word to be encrypted: ");
+                String inputText = input.next();
+
+                System.out.println("Great job, now enter the shift: ");
+                Integer inputShift = input.nextInt();
+
+                Cipher inputCaesarEncrypt = new Cipher(inputText, inputShift);
+                String cipherText = inputCaesarEncrypt.encrypt(inputText, inputShift);
+
+                System.out.println("Your encrypted word is " + cipherText);
+
+
